d65068293b2b9579327dcecf024a4331806c1995
heppy452 <heppy452@gmail.com>
2020-03-09 15:40:06 +0800

    add core

---
+++ app/views/skin/footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<br><hr class="footer">
+	<p align="center"> <?=$this->l_skin->apps_config('footer');?> </p>
+	<p align="center">Version <?=CI_VERSION;?> - Page rendered in <b>{elapsed_time}</b> second </p>
+</div>
