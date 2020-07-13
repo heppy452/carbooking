514278d9f5c82a45d8739f3f609937ea0c45c069
heppy452 <heppy452@gmail.com>
2020-07-13 13:48:53 +0800

    cookie

--- app/config/config.php
+++ app/config/config.php
@@ -380,7 +380,7 @@ $config['encryption_key'] = 'FCD9649AF3D993FE';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'imip_core4';
+$config['sess_cookie_name'] = 'carpool';
 $config['sess_expiration'] = 3600;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
